331bd39abaf8e53b63e660fb4c58ecf1bc4e8ac1
xuzhong <254018158@qq.com>
2017-07-11 19:20:42 +0800

    环境配置完成

---
+++ src/main/java/com/xuzhong/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.xuzhong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	@RequestMapping("/login")
+	public String login(){
+		
+		return "index";
+	}
+	
+
+}
